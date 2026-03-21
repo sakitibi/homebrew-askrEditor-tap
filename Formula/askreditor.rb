66e439e9a7a557a778ffb127876de11b0afbe371
sakitibi <sakitibi10nin@icloud.com>
2026-03-22 08:26:15 +0900

    Merge remote changes

--- Formula/askreditor.rb
+++ Formula/askreditor.rb
@@ -7,18 +7,18 @@ class Askreditor < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/sakitibi/askreditor/releases/download/v#{Version}/askreditor-#{Version}-darwin-arm64.tar.gz"
-      sha256 "c8c6ca6c5efa0771eb82188cd827fc956760555a55ee6232d2221bc98c372209"
+      sha256 "c56565aa5f4bd705cdecb7745b0adfd8b0c37f9021a5cde235806b259f2db5ec"
     else
       url "https://github.com/sakitibi/askreditor/releases/download/v#{Version}/askreditor-#{Version}-darwin-amd64.tar.gz"
-      sha256 "d5be9c4527aa9a7ab3d795f64b487cf23d52608d66eaeb712e812365677df10e"
+      sha256 "edea0f9da4405e97851fcd09b476b3311bb56c604297aad8c2935311dae99f56"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/sakitibi/askreditor/releases/download/v#{Version}/askreditor-#{Version}-linux-arm64.tar.gz"
-      sha256 "d998f6ab3deb3cdfca951f21c008f9f96f3b8d3637d76a1434700b7ea13baa39"
+      sha256 "d66c0a2eaa0d3b856d09e2bed7d0f2236813923266d987d0b30d56c96dae2ee6"
     else
       url "https://github.com/sakitibi/askreditor/releases/download/v#{Version}/askreditor-#{Version}-linux-amd64.tar.gz"
-      sha256 "a3e0d12d7baea2ffbd6cfc2efddcac00ab73fb01c95ec3e1912ea10be2186518"
+      sha256 "00610f295251b692861f1456c5a5de8d643159e5a3bd0685dee3c8f641d1d857"
     end
   end
 
